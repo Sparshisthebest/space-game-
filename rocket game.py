782f85f3bcfb70fa2ab317f1a2d373f536a6d118
Sparshisthebest <sparsh.nayak0812@gmail.com>
2026-04-08 19:28:14 +0100

    Update rocket game.py

---
+++ rocket game.py
@@ -0,0 +1,48 @@
+import math
+import random
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+PLAYER_START_X = 370
+PLAYER_START_Y = 380
+ENEMY_START_Y_MIN = 50
+ENEMY_START_Y_MAX = 150
+ENEMY_SPEED_X = 4
+ENEMY_SPEED_Y = 40
+BULLET_SPEED_Y = 10
+COLLISION_DISTANCE = 27
+pygame.init()
+screen = pygame.image.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+ackground = pygame.image.load('backgroung.png')
+pygame.display.set_caption("Space Invader" )
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+playerImg = pygame.image.load('player.png')
+playerX = PLAYER_START_X
+PLAYERY = PLAYER_START_Y
+playerX_change=0
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+for _i in range(num_of_enemies):
+    enemyImg.apend(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
+bulletImg = pygame.image.loas('bullet.png')
+bulletX = 0
+bulletY = PLAYER_START_Y
+bulletX_change = 0
+bulletY_change = BULLET_SPEED_Y
+bullet_state = 'ready'
+score_value = 0
+font = pygame.font.Font('freesanbold.ttf' , 32)
+textX = 10
+textY = 10
+over_font = pygame.font.Font("freesanbolt.ttf", 64)
+
